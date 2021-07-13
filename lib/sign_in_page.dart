ac57d0e017d7e2903df978540f4f2edbac750e18
Rahul Ramkumar <rahulram226@gmail.com>
2021-07-12 20:59:15 -0400

    Format sign_in_page.dart with Trailing Commas

--- lib/sign_in_page.dart
+++ lib/sign_in_page.dart
@@ -11,83 +11,101 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(children: [
-      Column(
+      body: Stack(
         children: [
-          Padding(
-              padding: EdgeInsets.only(top: 50.0),
-              child: Container(
-                height: 150,
-                width: 150,
-                decoration: BoxDecoration(
+          Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 50.0),
+                child: Container(
+                  height: 150,
+                  width: 150,
+                  decoration: BoxDecoration(
                     borderRadius: BorderRadius.all(Radius.circular(30.0)),
                     color: Colors.black12,
                     image: DecorationImage(
-                        image: AssetImage('assets/images/Logo.png'))),
-              )),
-          Container(
-              padding: EdgeInsets.only(top: 50.0, left: 20.0),
-              alignment: Alignment.topLeft,
-              child: Text(
-                "LOGIN",
-                style: TextStyle(
+                      image: AssetImage('assets/images/Logo.png'),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 50.0, left: 20.0),
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "LOGIN",
+                  style: TextStyle(
                     fontSize: 36.0,
                     fontFamily: 'Cornerstone',
-                    color: Color(0xFF113654)),
-              )),
-          Padding(
-              padding: EdgeInsets.only(top: 5.0, left: 20.0, right: 20.0),
-              child: TextField(
-                controller: emailController,
-                decoration: InputDecoration(
+                    color: Color(0xFF113654),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5.0, left: 20.0, right: 20.0),
+                child: TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
                     labelText: "Email",
                     filled: true,
-                    fillColor: Colors.black12),
-              )),
-          Padding(
-              padding: EdgeInsets.only(top: 15.0, left: 20.0, right: 20.0),
-              child: TextField(
-                controller: passwordController,
-                decoration: InputDecoration(
+                    fillColor: Colors.black12,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 15.0, left: 20.0, right: 20.0),
+                child: TextField(
+                  controller: passwordController,
+                  decoration: InputDecoration(
                     labelText: "Password",
                     filled: true,
-                    fillColor: Colors.black12),
-              )),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Padding(
-                  padding: EdgeInsets.only(right: 100),
-                  child: TextButton(
-                    child: Text('Forgot password?',
+                    fillColor: Colors.black12,
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(right: 100),
+                    child: TextButton(
+                      child: Text(
+                        'Forgot password?',
                         style: TextStyle(
-                            fontFamily: 'SpaceGrotesk',
-                            fontWeight: FontWeight.w600)),
-                    onPressed: () {},
-                  )),
-              TextButton(
-                child: Text('Create account',
-                    style: TextStyle(
+                          fontFamily: 'SpaceGrotesk',
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  TextButton(
+                    child: Text(
+                      'Create account',
+                      style: TextStyle(
                         fontFamily: 'SpaceGrotesk',
-                        fontWeight: FontWeight.w600)),
-                onPressed: () {},
-              )
-            ],
-          ),
-          Padding(
-              padding: EdgeInsets.only(top: 10, left: 300),
-              child: Container(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10, left: 300),
+                child: Container(
                   decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color(0xFFF4603D),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(.5),
-                          spreadRadius: 2,
-                          blurRadius: 8,
-                          offset: Offset(0, 2),
-                        )
-                      ]),
+                    shape: BoxShape.circle,
+                    color: Color(0xFFF4603D),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(.5),
+                        spreadRadius: 2,
+                        blurRadius: 8,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
                   child: IconButton(
                     icon: Icon(Icons.send),
                     iconSize: 20,
@@ -98,26 +116,33 @@ class SignInPage extends StatelessWidget {
                             passwordController.text.trim(),
                           );
                     },
-                  ))),
-          Container(
-              padding: EdgeInsets.only(top: 15),
-              width: 360,
-              child: ElevatedButton.icon(
-                icon: Icon(CustomIcons.google),
-                onPressed: () {},
-                label: Text('Sign in with Google'),
-              )),
-        ],
-      ),
-      Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 15),
+                width: 360,
+                child: ElevatedButton.icon(
+                  icon: Icon(CustomIcons.google),
+                  onPressed: () {},
+                  label: Text('Sign in with Google'),
+                ),
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
               height: 200,
               alignment: Alignment.bottomCenter,
               child: SvgPicture.asset(
                 'assets/images/mountain.svg',
                 alignment: Alignment.bottomCenter,
-              )))
-    ]));
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
