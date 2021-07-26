4291c426875ae2c1a54bc0206a104a7fa308d5d9
Rahul Ramkumar <rahulram226@gmail.com>
2021-07-26 12:40:06 -0400

    Add Rough Flavor Separation

---
+++ lib/app_entry.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
+import 'dart:io';
+
+import './authentication_service.dart';
+import './home_page.dart';
+import './sign_in_page.dart';
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+          create: (_) => AuthenticationService(FirebaseAuth.instance),
+        ),
+        // TODO: add initialData for nullSafety?
+        StreamProvider(
+          create: (context) =>
+              context.read<AuthenticationService>().authStateChanges,
+        ),
+      ],
+      child: MaterialApp(
+        title: 'HackPSU',
+        home: AuthenticationWrapper(),
+      ),
+    );
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
+class AuthenticationWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final firebaseUser = context.watch<User>();
+
+    if (firebaseUser != null) {
+      return HomePage();
+    }
+    return SignInPage();
+  }
+}
--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'dart:io';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
 import './app_entry.dart';
 import './flavor_constants.dart';
@@ -8,7 +8,7 @@ import './flavor_constants.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  Config.appFlavor = Flavor.PROD;
+  Config.appFlavor = Flavor.DEV;
   HttpOverrides.global = new MyHttpOverrides();
   runApp(MyApp());
 }
