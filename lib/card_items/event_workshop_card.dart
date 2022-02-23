9edf58387a134da9f3ccd098bb3cd100b9644951
Jonathan Tran <jaytran505@gmail.com>
2022-02-23 01:29:31 -0500

    began basic layout of event workshop card

--- lib/card_items/event_workshop_card.dart
+++ lib/card_items/event_workshop_card.dart
@@ -1,22 +1,52 @@
 import 'package:flutter/material.dart';
+import '../widgets/default_text.dart';
 import '../models/event.dart';
 import '../data/api.dart';
 
 // TODO: Actually work on displaying event details
 class EventWorkshopCard extends StatelessWidget {
+  bool favorite = false;
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Event>>(
-      future: Api.getEvents(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          // return Text(snapshot.data[0].eventTitle);
-        } else if (snapshot.hasError) {
-          return Text('${snapshot.error}');
-        }
-
-        return const CircularProgressIndicator();
-      },
-    );
+    return Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        child: InkWell(
+          onTap: () {
+            debugPrint("Description");
+          },
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width - 50,
+            height: 80,
+            child: Row(children: [
+              Padding(padding: EdgeInsets.only(left: 8)),
+              Icon(
+                Icons.stars,
+                size: 50,
+                color: Colors.red,
+              ),
+              Column(children: [
+                Padding(padding: EdgeInsets.only(top: 15, left: 100)),
+                Text(
+                  "eventLocation",
+                ),
+                Text("eventTitle")
+              ]),
+              Container(
+                  padding: EdgeInsets.only(left: 150),
+                  child: IconButton(
+                    onPressed: () {
+                      favorite = !favorite;
+                    },
+                    icon: favorite
+                        ? Icon(Icons.favorite, color: Colors.red)
+                        : Icon(
+                            Icons.favorite_border,
+                          ),
+                    iconSize: 30,
+                    splashRadius: 22,
+                  )),
+            ]),
+          ),
+        ));
   }
 }
