1ac739212a288b079ded548e1ac22a4f5bff95b2
Jonathan Tran <jaytran505@gmail.com>
2022-03-20 12:14:29 -0400

    all font and formatting complete

--- lib/card_items/event_workshop_card.dart
+++ lib/card_items/event_workshop_card.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import '../widgets/default_text.dart';
+import 'package:flutter/rendering.dart';
 import '../models/event.dart';
+import '../widgets/default_text.dart';
 
-// TODO: Actually work on displaying event details
 class EventWorkshopCard extends StatelessWidget {
   final Event event;
 
@@ -10,6 +10,17 @@ class EventWorkshopCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String parseLocation(String locationName) {
+      if (locationName.contains('zoom')) {
+        return "Zoom";
+      } else if (locationName.contains('youtube') ||
+          locationName.contains('youtu.be')) {
+        return "Youtube";
+      } else {
+        return locationName;
+      }
+    }
+
     return Card(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
         child: InkWell(
@@ -17,28 +28,49 @@ class EventWorkshopCard extends StatelessWidget {
             debugPrint(event.eventDescription);
           },
           child: SizedBox(
-            width: MediaQuery.of(context).size.width - 50,
             height: 80,
             child: Row(children: [
-              Padding(padding: EdgeInsets.only(left: 8.0)),
-              Icon(
-                Icons.stars,
-                size: 50,
-                color: Colors.red,
+              const Padding(padding: EdgeInsets.only(left: 10.0)),
+              if (event.eventIcon != null)
+                CircleAvatar(
+                    radius: 25, backgroundImage: NetworkImage(event.eventIcon)),
+              const Padding(padding: EdgeInsets.only(right: 10.0)),
+
+              Expanded(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      DefaultText(
+                        parseLocation(event.locationName),
+                        color: const Color(0x99000000),
+                        fontLevel: TextLevel.overline,
+                      ),
+                      DefaultText(
+                        event.eventTitle,
+                        fontLevel: TextLevel.sub1,
+                      ),
+                      if (event.wsPresenterNames == null)
+                        Center(
+                            child: DefaultText('• • •',
+                                color: const Color(0x57000000))),
+                      if (event.wsPresenterNames != null)
+                        DefaultText(event.wsPresenterNames ?? '',
+                            color: const Color(0xFF6A85B9),
+                            fontLevel: TextLevel.body2)
+                    ]),
               ),
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                Padding(padding: EdgeInsets.only(top: 10.0)),
-                DefaultText(
-                  event.locationName.contains('zoom')
-                      ? "Zoom"
-                      : event.locationName,
-                  fontLevel: TextLevel.overline,
-                ),
-                DefaultText(
-                  event.eventTitle,
-                  fontLevel: TextLevel.sub1,
-                )
-              ]),
+              SizedBox(
+                child: Align(
+                    alignment: Alignment.centerRight,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.favorite, color: Colors.red),
+                      iconSize: 30,
+                      splashRadius: 20.0,
+                    )),
+              ),
+
               // TODO: add heart/starred function
               // Container(
               //     padding: EdgeInsets.only(left: 120),
