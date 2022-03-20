ff15093e65baa9dd27b9ec35ddfa1eb8e27b9b9b
Jonathan Tran <jaytran505@gmail.com>
2022-03-20 14:09:27 -0400

    detail popup implemented. need to add content

--- lib/card_items/event_workshop_card.dart
+++ lib/card_items/event_workshop_card.dart
@@ -22,72 +22,67 @@ class EventWorkshopCard extends StatelessWidget {
     }
 
     return Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-        child: InkWell(
-          onTap: () {
-            debugPrint(event.eventDescription);
-          },
-          child: SizedBox(
-            height: 80,
-            child: Row(children: [
-              const Padding(padding: EdgeInsets.only(left: 10.0)),
-              if (event.eventIcon != null)
-                CircleAvatar(
-                    radius: 25, backgroundImage: NetworkImage(event.eventIcon)),
-              const Padding(padding: EdgeInsets.only(right: 10.0)),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      child: InkWell(
+        onTap: () {
+          showModalBottomSheet<void>(
+            context: context,
+            builder: (BuildContext context) {
+              // TODO: add content and formatting
+              return Container(
+                height: 300,
+              );
+            },
+          );
+        },
+        child: SizedBox(
+          height: 80,
+          child: Row(children: [
+            const Padding(padding: EdgeInsets.only(left: 10.0)),
+            if (event.eventIcon != null)
+              CircleAvatar(
+                  radius: 25, backgroundImage: NetworkImage(event.eventIcon)),
+            const Padding(padding: EdgeInsets.only(right: 10.0)),
 
-              Expanded(
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      DefaultText(
-                        parseLocation(event.locationName),
-                        color: const Color(0x99000000),
-                        fontLevel: TextLevel.overline,
-                      ),
-                      DefaultText(
-                        event.eventTitle,
-                        fontLevel: TextLevel.sub1,
-                      ),
-                      if (event.wsPresenterNames == null)
-                        Center(
-                            child: DefaultText('• • •',
-                                color: const Color(0x57000000))),
-                      if (event.wsPresenterNames != null)
-                        DefaultText(event.wsPresenterNames ?? '',
-                            color: const Color(0xFF6A85B9),
-                            fontLevel: TextLevel.body2)
-                    ]),
-              ),
-              SizedBox(
-                child: Align(
-                    alignment: Alignment.centerRight,
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.favorite, color: Colors.red),
-                      iconSize: 30,
-                      splashRadius: 20.0,
-                    )),
-              ),
-
-              // TODO: add heart/starred function
-              // Container(
-              //     padding: EdgeInsets.only(left: 120),
-              //     child: IconButton(
-              //       onPressed: () {
-              //         event.starred = !event.starred;
-              //       },
-              //       icon: event.starred
-              //           ? Icon(Icons.favorite, color: Colors.red)
-              //           : Icon(
-              //               Icons.favorite_border,
-              //             ),
-              //       iconSize: 30,
-              //       splashRadius: 22,
-              //     )),
-            ]),
-          ),
-        ));
+            Expanded(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    DefaultText(
+                      parseLocation(event.locationName),
+                      color: const Color(0x99000000),
+                      fontLevel: TextLevel.overline,
+                    ),
+                    DefaultText(
+                      event.eventTitle,
+                      fontLevel: TextLevel.sub1,
+                    ),
+                    if (event.wsPresenterNames == null)
+                      Center(
+                          child: DefaultText('• • •',
+                              color: const Color(0x57000000))),
+                    if (event.wsPresenterNames != null)
+                      DefaultText(event.wsPresenterNames ?? '',
+                          color: const Color(0xFF6A85B9),
+                          fontLevel: TextLevel.body2)
+                  ]),
+            ),
+            // TODO: add the change in event.starred function
+            SizedBox(
+              child: Align(
+                  alignment: Alignment.centerRight,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.favorite, color: Colors.red),
+                    iconSize: 30,
+                    splashRadius: 20.0,
+                  )),
+            ),
+            const Padding(padding: EdgeInsets.only(right: 15.0))
+          ]),
+        ),
+      ),
+    );
   }
 }
