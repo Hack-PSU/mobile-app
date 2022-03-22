2d9c69127a34ef9afbcabbcad485b43c0dd14b77
Jonathan Tran <jaytran505@gmail.com>
2022-03-22 03:50:23 -0400

    adding content to popup. need time and location

--- lib/card_items/event_workshop_card.dart
+++ lib/card_items/event_workshop_card.dart
@@ -27,10 +27,87 @@ class EventWorkshopCard extends StatelessWidget {
         onTap: () {
           showModalBottomSheet<void>(
             context: context,
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(10))),
             builder: (BuildContext context) {
               // TODO: add content and formatting
               return Container(
+                padding: const EdgeInsets.all(15.0),
                 height: 300,
+                child: Column(
+                  children: [
+                    Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          if (event.eventIcon != null)
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 5.0, right: 10.0),
+                              child: CircleAvatar(
+                                  radius: 25,
+                                  backgroundImage:
+                                      NetworkImage(event.eventIcon)),
+                            ),
+
+                          Expanded(
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  DefaultText(
+                                    event.eventTitle,
+                                    fontLevel: TextLevel.h4,
+                                  ),
+                                  if (event.wsPresenterNames != null)
+                                    DefaultText(event.wsPresenterNames ?? '',
+                                        color: const Color(0xFF6A85B9),
+                                        fontLevel: TextLevel.body2),
+                                  DefaultText("6:00pm - 7:00pm",
+                                      fontLevel: TextLevel.body2,
+                                      color: const Color(0xFF6A85B9)),
+                                ]),
+                          ),
+                          // TODO: add the change in event.starred function
+                          SizedBox(
+                            child: Align(
+                              alignment: Alignment.topRight,
+                              child: IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.favorite,
+                                    color: Colors.red),
+                                iconSize: 30,
+                                splashRadius: 20.0,
+                              ),
+                            ),
+                          ),
+                        ]),
+                    DefaultText(event.locationName),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.only(top: 10.0),
+                      padding: const EdgeInsets.only(
+                          left: 10.0, right: 10.0, top: 5.0, bottom: 10.0),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                            width: 4,
+                            color: Colors.black12,
+                          ),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(10))),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            DefaultText(
+                              'Description',
+                              fontLevel: TextLevel.body1,
+                            ),
+                            DefaultText(
+                              event.eventDescription,
+                              fontLevel: TextLevel.body2,
+                            ),
+                          ]),
+                    )
+                  ],
+                ),
               );
             },
           );
@@ -38,10 +115,12 @@ class EventWorkshopCard extends StatelessWidget {
         child: SizedBox(
           height: 80,
           child: Row(children: [
-            const Padding(padding: EdgeInsets.only(left: 10.0)),
             if (event.eventIcon != null)
-              CircleAvatar(
-                  radius: 25, backgroundImage: NetworkImage(event.eventIcon)),
+              Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: CircleAvatar(
+                      radius: 25,
+                      backgroundImage: NetworkImage(event.eventIcon))),
             const Padding(padding: EdgeInsets.only(right: 10.0)),
 
             Expanded(
@@ -71,15 +150,16 @@ class EventWorkshopCard extends StatelessWidget {
             // TODO: add the change in event.starred function
             SizedBox(
               child: Align(
-                  alignment: Alignment.centerRight,
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.favorite, color: Colors.red),
-                    iconSize: 30,
-                    splashRadius: 20.0,
-                  )),
+                alignment: Alignment.centerRight,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.favorite, color: Colors.red),
+                  iconSize: 30,
+                  splashRadius: 20.0,
+                ),
+              ),
             ),
-            const Padding(padding: EdgeInsets.only(right: 15.0))
+            const Padding(padding: EdgeInsets.only(right: 10.0))
           ]),
         ),
       ),
