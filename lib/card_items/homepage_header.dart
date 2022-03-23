839ec31619ff47322a8ffe940414694b763cb36e
Kyle D'Arcy <kyle.darcy@icloud.com>
2022-03-23 19:27:54 -0400

    finished homepage header

--- lib/card_items/homepage_header.dart
+++ lib/card_items/homepage_header.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+import '../styles/theme_colors.dart';
+import '../widgets/button.dart';
 import '../widgets/default_text.dart';
+import 'countdown_timer_card.dart';
 
 class HomepageHeader extends StatelessWidget {
   const HomepageHeader({Key key}) : super(key: key);
@@ -9,88 +13,77 @@ class HomepageHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return
-      Container(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Center(
-              child: AspectRatio(
-                aspectRatio: 487 / 560,
-                  child: Container(
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                          fit: BoxFit.fitWidth,
-                          alignment: FractionalOffset.topCenter,
-                          image: AssetImage('assets/images/background.jpg'),
-                        )
-                    ),
-                  ),
-              ),
-          ),
-          SvgPicture.asset('assets/images/mountain2.svg',
-           width: MediaQuery.of(context).size.width,
-          ),
-            Positioned.fill(
-              child: Align(
-                alignment: Alignment.bottomLeft,
-                child: Container(
-                  width: double.infinity,
-                  height: 0,
-                  color: Colors.black,
-                  child: Text("Hello!")
+      Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          AspectRatio(
+            aspectRatio: 487 / 560,
+              child: Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.fitWidth,
+                      alignment: FractionalOffset.topCenter,
+                      image: AssetImage('assets/images/background.jpg'),
+                    )
                 ),
               ),
-            ),
-          Column(
-            // mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              DefaultText("\n2 Hours left!", textLevel: TextLevel.h4, color: Colors.white, fontSize: 40,)
-            ],
-          )
-          ],
+          ),
+        SvgPicture.asset('assets/images/mountain2.svg',
+         width: MediaQuery.of(context).size.width,
         ),
-      );
-
-Column(
-children: [
-  Stack(
-    alignment: Alignment.center,
-    children: [
-      Image(
-        image: const AssetImage('assets/images/background.jpg'),
-      ),
-      Column(
-        children: [
-          Row(children: [
-            Container(height: 400, width: 2000, color: Colors.blue),
-          ]),
-          Row(children: [
-            SvgPicture.asset('assets/images/mountain2.svg',
-                fit: BoxFit.fitHeight),
-          ])
-        ],
-      ),
-      Container(
-        alignment: Alignment.center,
-        child: Padding(
-          padding: const EdgeInsets.only(top: 20.0),
-          child: Container(
-            height: 500,
-            width: 500,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(30.0)),
-              color: Colors.white12,
-              image: DecorationImage(
-                image: AssetImage('assets/images/Logo.png'),
+        Positioned.fill(
+          child:Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 10.0, bottom: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CountdownTimerCard(),
+                  Row(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () async {
+                          await launch("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+                          // Respond to button press
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Colors.white)
+                        ),
+                        child: Row(
+                          children: [
+                            Icon(Icons.favorite, color: ThemeColors.StadiumOrange,),
+                            Container(width: 10,),
+                            DefaultText("SUBMIT", textLevel: TextLevel.button, color: ThemeColors.StadiumOrange,),
+                          ],
+                        ),
+                      ),
+                      Container(width: 20,),
+                      ElevatedButton(
+                        onPressed: () async {
+                          await launch("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+                          // Respond to button press
+                        },
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(Color(0xFF6A85B9))
+                        ),
+                        child: Row(
+                          children: [
+                            Icon(Icons.discord),
+                            Container(width: 10,),
+                            DefaultText("DISCORD", textLevel: TextLevel.button, color: Color(0xFFFFFFFF),),
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                ],
               ),
             ),
           ),
         ),
-      )
-    ],
-  ),
-],
-);
+      ],
+      );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -15,107 +15,130 @@ import '../widgets/button.dart';
 import '../widgets/default_text.dart';
 import '../widgets/screen.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Screen(
-      withBottomNavigation: true,
-      header: ScreenHeader.only(
-        withProfile: true,
-      ),
-      body: const HomeScreen(),
-    );
-  }
+  _HomePageState createState() => _HomePageState();
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key key}) : super(key: key);
+class _HomePageState extends State<HomePage> {
+  ScrollController _controller;
+  double _logoWidth;
+  double _logoHeight;
+
+  _scrollListener() {
+    if (_controller.position.pixels == 0) {
+      setState(() {
+        _logoWidth = 135;
+        _logoHeight = 135;
+      });
+    } else {
+      setState(() {
+        _logoWidth = 50;
+        _logoHeight = 50;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    _controller = ScrollController();
+    _logoWidth = 135;
+    _logoHeight = 135;
+    _controller.addListener(_scrollListener);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<EventCubit, List<Event>>(
-      builder: (context, events) {
-        return BlocBuilder<RegistrationCubit, List<Registration>>(
-          builder: (context, registrations) {
-            // fetch data first
-            // context.read<EventCubit>().getEvents();
-            // context.read<RegistrationCubit>().getUserInfo();
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: [
+        Screen(
+          withBottomNavigation: true,
+          header: ScreenHeader.only(
+            withProfile: true,
+          ),
+          body: BlocBuilder<EventCubit, List<Event>>(
+            builder: (context, events) {
+              return BlocBuilder<RegistrationCubit, List<Registration>>(
+                builder: (context, registrations) {
+                  // fetch data first
+                  // context.read<EventCubit>().getEvents();
+                  // context.read<RegistrationCubit>().getUserInfo();
 
-            // while loading show the progress indicator
-            if (events == null || registrations == null) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
+                  // while loading show the progress indicator
+                  // if (events == null || registrations == null) {
+                  //   return const Center(
+                  //     child: CircularProgressIndicator(),
+                  //   );
+                  // }
 
-            return Column(
-              children: [
-                AppBar(
-                  backgroundColor: Colors.white,
-                  actions: [
-                    Padding(
-                      padding: const EdgeInsets.only(right: 8.0),
-                      child: CircleAvatar(radius: 40),
-                    )
-                  ],
-                  flexibleSpace: SafeArea(
-                      child: Center(
-                        child: Padding(
-                          padding: const EdgeInsets.only(top: 20.0, bottom: 10.0),
-                          child: Container(
-                            height: 100,
-                            width: 100,
-                            decoration: const BoxDecoration(
-                              borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                              color: Color(0xffF5F5F5),
-                              image: DecorationImage(
-                                image: AssetImage('assets/images/Logo.png'),
+                  return Column(
+                    children: [
+                      Expanded(
+                        child: ListView(
+                          controller: _controller,
+                          children: [
+                            Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const HomepageHeader(),
+                                  // registrations is passed in from here
+                                  // UserPinCard(registrations),
+                                  // ...events
+                                  //     .map((e) => DefaultText(
+                                  //           e.eventTitle ?? "Event",
+                                  //           fontSize: 14,
+                                  //         ))
+                                  //     .toList(),
+                                  Button(
+                                    variant: ButtonVariant.TextButton,
+                                    onPressed: () {
+                                      context.read<AuthenticationRepository>().signOut();
+                                    },
+                                    child: DefaultText(
+                                      "Log out",
+                                    ),
+                                  ),
+                                  Text("hello"),
+                                  Text("hello"),
+                                  Text("hello"),
+                                  Text("hello"),
+                                  Text("hello"),
+                                ],
                               ),
                             ),
-                          ),
-                        ),
-                      )
-                  ),
-                ),
-                Expanded(
-                  child: ListView(
-                    children: [
-                      Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            HomepageHeader(),
-                            CountdownTimerCard(),
-                            // registrations is passed in from here
-                            UserPinCard(registrations),
-                            ...events
-                                .map((e) => DefaultText(
-                                      e.eventTitle ?? "Event",
-                                      fontSize: 14,
-                                    ))
-                                .toList(),
-                            Button(
-                              variant: ButtonVariant.TextButton,
-                              onPressed: () {
-                                context.read<AuthenticationRepository>().signOut();
-                              },
-                              child: DefaultText(
-                                "Log out",
-                              ),
-                            )
                           ],
                         ),
                       ),
                     ],
-                  ),
+                  );
+                },
+              );
+            },
+          ),
+        ),
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20.0, bottom: 10.0),
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              height: _logoHeight,
+              width: _logoWidth,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                color: Color(0xffF5F5F5),
+                image: DecorationImage(
+                  image: AssetImage('assets/images/Logo.png'),
                 ),
-              ],
-            );
-          },
-        );
-      },
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
