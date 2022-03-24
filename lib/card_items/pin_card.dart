b85ccdaebe690cb73eb3809937c7e2e62070de2b
Kalp Patel <kalppatel.3311@gmail.com>
2022-03-24 01:16:42 -0400

    "Show QR code for Pin [HACKPSU_(pin - basePin]"

--- lib/card_items/pin_card.dart
+++ lib/card_items/pin_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 import '../models/registration.dart';
 
@@ -16,9 +17,44 @@ class UserPinCard extends StatelessWidget {
     if (_data != null) {
       _data.sort((a, b) => (a.time).compareTo(b.time));
       try {
-        return Text(
-          "Your Pin: ${(_data.last.pin - _data.last.basePin).toString()}",
-        );
+        return InkWell(
+            onTap: () {
+              showGeneralDialog(
+                  context: context,
+                  barrierDismissible: true,
+                  barrierLabel: MaterialLocalizations.of(context)
+                      .modalBarrierDismissLabel,
+                  barrierColor: Colors.black45,
+                  transitionDuration: const Duration(milliseconds: 200),
+                  pageBuilder: (BuildContext buildContext, animation,
+                      secondaryAnimation) {
+                    return GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Container(
+                            alignment: Alignment.center,
+                            padding: const EdgeInsets.all(20.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                              color: Colors.white,
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                QrImage(
+                                  data:
+                                      "HACKPSU_${_data.last.pin - _data.last.basePin}",
+                                  version: 3,
+                                )
+                              ],
+                            )));
+                  });
+            },
+            child: Text(
+              "Your Pin: ${(_data.last.pin - _data.last.basePin).toString()}",
+            ));
       } on StateError catch (e) {
         return const Text(
           "No Registration PIN because no registration data was found",
