083991be27da4f791f897bf85c61d53b709ebb59
Kalp Patel <kalppatel.3311@gmail.com>
2022-03-25 00:49:09 -0400

    Add items to fill white-space

--- lib/card_items/pin_card.dart
+++ lib/card_items/pin_card.dart
@@ -13,7 +13,6 @@ class UserPinCard extends StatelessWidget {
         super(key: key);
 
   final List<Registration> _data;
-//"My PIN \n${(_data.last.pin - _data.last.basePin).toString()}",
   @override
   Widget build(BuildContext context) {
     if (_data != null) {
@@ -63,62 +62,80 @@ class UserPinCard extends StatelessWidget {
                   const EdgeInsets.only(top: 10.0, bottom: 10.0, left: 20.0),
               width: MediaQuery.of(context).size.width * 0.95,
               alignment: Alignment.centerLeft,
-              child: Container(
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                    DefaultText(
-                      "My PIN",
-                      textLevel: TextLevel.h4,
-                    ),
-                    Container(height: 10.0),
-                    DefaultText(
-                      "${(_data.last.pin - _data.last.basePin).toString()}",
-                      textLevel: TextLevel.h1,
-                    )
-                  ]))),
+              child: Row(children: [
+                Container(
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                      DefaultText(
+                        "My PIN",
+                        textLevel: TextLevel.h4,
+                      ),
+                      Container(height: 10.0),
+                      DefaultText(
+                        "${(_data.last.pin - _data.last.basePin).toString()}",
+                        textLevel: TextLevel.h1,
+                      )
+                    ])),
+                Container(
+                    padding: EdgeInsets.only(
+                        left: MediaQuery.of(context).size.width * .52),
+                    child: QrImage(
+                      data: "HACKPSU_${_data.last.pin - _data.last.basePin}",
+                      version: 3,
+                      size: 80,
+                    ))
+              ])),
         );
       } on StateError catch (e) {
         return Container(
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5.0), color: Colors.white),
+                borderRadius: BorderRadius.circular(10.0), color: Colors.white),
             margin: const EdgeInsets.only(top: 10),
             padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
             width: MediaQuery.of(context).size.width * 0.95,
             alignment: Alignment.centerLeft,
-            child: Column(children: [
-              DefaultText(
-                "My PIN",
-                textLevel: TextLevel.h4,
-              ),
-              Container(
-                  padding: const EdgeInsets.only(top: 10.0, left: 15.0),
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      const url = 'https://app.hackpsu.org/register';
+            child: Row(children: [
+              Column(children: [
+                DefaultText(
+                  "My PIN",
+                  textLevel: TextLevel.h4,
+                ),
+                Container(
+                    padding: const EdgeInsets.only(top: 5.0, left: 15.0),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        const url = 'https://app.hackpsu.org/register';
 
-                      if (await canLaunch(url)) {
-                        await launch(
-                          url,
-                          forceWebView: true,
-                          enableJavaScript: true,
-                          enableDomStorage: true,
-                        );
-                      } else {
-                        throw Exception('Could not launch $url');
-                      }
-                    },
-                    style: ButtonStyle(
-                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0))),
-                        backgroundColor:
-                            MaterialStateProperty.all(const Color(0xFF6A85B9))),
-                    child: DefaultText(
-                      "Register",
-                      textLevel: TextLevel.body2,
-                      color: Colors.white,
-                    ),
-                  )),
+                        if (await canLaunch(url)) {
+                          await launch(
+                            url,
+                            forceWebView: true,
+                            enableJavaScript: true,
+                            enableDomStorage: true,
+                          );
+                        } else {
+                          throw Exception('Could not launch $url');
+                        }
+                      },
+                      style: ButtonStyle(
+                          shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0))),
+                          backgroundColor: MaterialStateProperty.all(
+                              const Color(0xFF6A85B9))),
+                      child: DefaultText(
+                        "Register",
+                        textLevel: TextLevel.body2,
+                        color: Colors.white,
+                      ),
+                    )),
+              ]),
+              Container(width: 40.0),
+              Flexible(
+                  child: DefaultText(
+                      "Please register to participate at the event.")),
+              Container(width: 15.0),
             ]));
       } catch (e) {
         return DefaultText(
