1b808a2cccb852382c8a62d3601c87e59421616e
susanto-tm <susanto.tm@gmail.com>
2022-04-01 23:22:41 -0400

    Added EECS png and migrated svg files to cloud storage

--- lib/card_items/sponsor_carousel.dart
+++ lib/card_items/sponsor_carousel.dart
@@ -13,6 +13,21 @@ const LION_LAUNCHPAD_SVG = "assets/images/sponsors/Lion-Launchpad.svg";
 const MICROSOFT_SVG = "assets/images/sponsors/Microsoft_original.svg";
 const NITTANY_AI_SVG = "assets/images/sponsors/nittanyai-day.svg";
 
+const ECHO_AR_SVG_URL =
+    "https://firebasestorage.googleapis.com/v0/b/hackpsu18.appspot.com/o/sponsorship-logos%2FEchoAR-day.svg?alt=media";
+const EECS_SVG_URL =
+    "https://firebasestorage.googleapis.com/v0/b/hackpsu18.appspot.com/o/sponsorship-logos%2FEECS-day.png?alt=media";
+const HVC_SVG_URL =
+    "https://firebasestorage.googleapis.com/v0/b/hackpsu18.appspot.com/o/sponsorship-logos%2FHVC.svg?alt=media";
+const ICDS_SVG_URL =
+    "https://firebasestorage.googleapis.com/v0/b/hackpsu18.appspot.com/o/sponsorship-logos%2FICDS-day.svg?alt=media";
+const LION_LAUNCHPAD_SVG_URL =
+    "https://firebasestorage.googleapis.com/v0/b/hackpsu18.appspot.com/o/sponsorship-logos%2FLion-Launchpad.svg?alt=media";
+const MICROSOFT_SVG_URL =
+    "https://firebasestorage.googleapis.com/v0/b/hackpsu18.appspot.com/o/sponsorship-logos%2FMicrosoft_original.svg?alt=media";
+const NITTANY_AI_SVG_URL =
+    "https://firebasestorage.googleapis.com/v0/b/hackpsu18.appspot.com/o/sponsorship-logos%2Fnittanyai-day.svg?alt=media";
+
 const ECHO_AR_URL = "https://www.echoar.xyz/";
 const EECS_URL = "https://www.eecs.psu.edu/";
 const HVC_URL = "https://www.linkedin.com/company/happy-valley-communications/";
@@ -45,13 +60,13 @@ class SponsorCarousel extends StatelessWidget {
               enlargeCenterPage: true,
             ),
             items: [
-              [ECHO_AR_SVG, ECHO_AR_URL],
-              [EECS_SVG, EECS_URL],
-              [HVC_SVG, HVC_URL],
-              [ICDS_SVG, ICDS_URL],
-              [LION_LAUNCHPAD_SVG, LION_LAUNCHPAD_URL],
-              [MICROSOFT_SVG, MICROSOFT_URL],
-              [NITTANY_AI_SVG, NITTANY_AI_URL]
+              [ECHO_AR_SVG_URL, ECHO_AR_URL],
+              [EECS_SVG_URL, EECS_URL],
+              [HVC_SVG_URL, HVC_URL],
+              [ICDS_SVG_URL, ICDS_URL],
+              [LION_LAUNCHPAD_SVG_URL, LION_LAUNCHPAD_URL],
+              [MICROSOFT_SVG_URL, MICROSOFT_URL],
+              [NITTANY_AI_SVG_URL, NITTANY_AI_URL]
             ].map(
               (i) {
                 return Builder(
@@ -59,27 +74,34 @@ class SponsorCarousel extends StatelessWidget {
                     return InkWell(
                       onTap: () async {
                         if (!await launch(i[1])) {
-                          throw "Could not launch $i[1]";
+                          throw Exception("Could not launch $i[1]");
                         }
                       },
                       child: Container(
                         width: MediaQuery.of(context).size.width,
                         margin: const EdgeInsets.symmetric(horizontal: 5.0),
                         decoration: const BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10.0),
-                              topRight: Radius.circular(10.0),
-                              bottomLeft: Radius.circular(10.0),
-                              bottomRight: Radius.circular(10.0),
-                            )),
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10.0),
+                            topRight: Radius.circular(10.0),
+                            bottomLeft: Radius.circular(10.0),
+                            bottomRight: Radius.circular(10.0),
+                          ),
+                        ),
                         child: Stack(
                           alignment: Alignment.center,
                           children: [
-                            SvgPicture.asset(
-                              i[0],
-                              width: MediaQuery.of(context).size.width * 0.70,
-                            )
+                            if (i[0].contains("png"))
+                              Image.network(
+                                i[0],
+                                width: MediaQuery.of(context).size.width * 0.7,
+                              ),
+                            if (i[0].contains("svg"))
+                              SvgPicture.network(
+                                i[0],
+                                width: MediaQuery.of(context).size.width * 0.7,
+                              ),
                           ],
                         ),
                       ),
