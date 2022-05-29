3c5119929fb70017317cb566b040b883c2e8464e
susanto-tm <susanto.tm@gmail.com>
2022-05-29 00:49:06 -0400

    Added updated prod and dev app signing to attempt to solve Firebase App Check error.

--- android/app/src/main/kotlin/com/example/hackpsu/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/hackpsu/MainActivity.kt
@@ -1,6 +1,20 @@
 package com.example.hackpsu
 
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        FirebaseApp.initializeApp(/*context=*/this)
+        Log.e("MainActivity", "onCreate")
+        val firebaseAppCheck = FirebaseAppCheck.getInstance()
+        firebaseAppCheck.installAppCheckProviderFactory(
+            DebugAppCheckProviderFactory.getInstance()
+        )
+        super.onCreate(savedInstanceState)
+    }
 }
